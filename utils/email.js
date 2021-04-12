467fb571f7712474bf9303bd1d1520933685b76c
Peter <pitiyke@gmail.com>
2021-04-12 13:21:00 +0100

    wrapped in try and catch block

--- utils/email.js
+++ utils/email.js
@@ -2,25 +2,28 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   //create nodemailer transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.USER_NAME,
-      pass: process.env.API_PASSWORD,
-    },
-  });
+  try {
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.USER_NAME,
+        pass: process.env.API_PASSWORD,
+      },
+    });
 
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.email,
-    subject: options.subject,
-    html: options.html,
-    text: options.message,
-  };
+    const mailOptions = {
+      from: process.env.EMAIL_FROM,
+      to: options.email,
+      subject: options.subject,
+      html: options.html,
+      text: options.message,
+    };
 
-  //sending the email
-  await transporter.sendMail(mailOptions);
+    //sending the email
+    await transporter.sendMail(mailOptions);
+  } catch (err) {
+    console.log(err);
+  }
 };
-
 module.exports = sendEmail;
